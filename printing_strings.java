32fcf589177e8ad86d58e0d3a3731e2a970b0032
wilson0g <1realbossbaby@gmail.com>
2025-08-13 04:25:55 +0000

    First Java code on printing strings

---
+++ printing_strings.java
@@ -0,0 +1,7 @@
+public class printing_strings  {
+    // This is my first java code!
+    public static void main (String[] args){
+        System.out.println("This is my first java code");
+        System.out.println("The world");
+    }
+}
