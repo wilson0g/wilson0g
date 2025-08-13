cd62c1c5a20f60d91d7fb2c9c034725bb48beb1d
wilson0g <1realbossbaby@gmail.com>
2025-08-13 13:48:07 +0000

    Variables in Java

---
+++ practice_on_variables.java
@@ -0,0 +1,23 @@
+
+public class practice_on_variables{
+    public static void main(String[]agrs){
+        int my_age = 18;
+        System.out.println("Kofi is " +  my_age);
+        double average_height = 10.5;
+        System.out.println("The average height of 6 boys is " + average_height);
+        char my_grade = 'A';
+        System.out.println("Computing grade: " + my_grade);
+        boolean is_a_beginner = false;
+        if (is_a_beginner==true){
+            System.out.println("As a beginner,constantly practice");
+        }
+        else{
+            System.err.println("You are not a beginner but still practice");
+        }
+        String name = "Wilson";
+        System.out.println("Hello " + name);
+
+    }
+         
+}
+    
